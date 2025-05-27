f335e2ff2c24298f34b49275910e148b972e782a
Bayleigh Andersen <bayleighandersen@Bayleighs-MacBook-Air.local>
2025-05-27 14:42:30 -0400

    header done

---
+++ server.js
@@ -0,0 +1,19 @@
+//Main server file for the application
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+//Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Serve index.html by default 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+//Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
