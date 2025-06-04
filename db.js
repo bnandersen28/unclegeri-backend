bd1a296547a22746080bd0c06c63a30641b65217
Bayleigh Andersen <bayleighandersen@Bayleighs-MacBook-Air.local>
2025-06-04 13:23:50 -0400

    hardcoded

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'unclegeri-db.c2vos4siyf8o.us-east-1.rds.amazonaws.com',
   user: 'admin',
-  password: process.env.DB_PASSWORD || 'UncleGeri$!', // Use environment variable or default password
+  password: 'UncleGeri$!', // Use environment variable or default password
   database: 'unclegeri',
   port: 3306,
   waitForConnections: true,
