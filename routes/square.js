ea686c0413d9339900e3f6d564e71f45656acb63
Bayleigh Andersen <bayleighandersen@Bayleighs-MacBook-Air.local>
2025-06-04 14:32:53 -0400

    added debug statements for square

--- routes/square.js
+++ routes/square.js
@@ -2,6 +2,11 @@ const { ApiError, Client, Environment } = require('square');
 
 const { isProduction, SQUARE_ACCESS_TOKEN } = require('./config');
 
+console.log('Initializing Square Client...');
+console.log('SQUARE_ACCESS_TOKEN exists:', !!process.env.SQUARE_ACCESS_TOKEN);
+console.log('First 5 characters of token:', process.env.SQUARE_ACCESS_TOKEN?.substring(0, 5));
+console.log('Using environment:', process.env.NODE_ENV);
+
 const client = new Client({
   environment: isProduction ? Environment.Production : Environment.Sandbox,
   accessToken: SQUARE_ACCESS_TOKEN,
